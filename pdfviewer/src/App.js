16622144f42ecaa21477c1b2f17325d70d8da7e3
suchir-cyber <suchirpandula@gmail.com>
2024-11-15 00:41:06 +0530

    created App.js main page

--- pdfviewer/src/App.js
+++ pdfviewer/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PDFViewer from './PDFViewer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PdfViewer</h1>
+      <h1>Admin's View</h1>
+      <PDFViewer />
     </div>
   );
 }
